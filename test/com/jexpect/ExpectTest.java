f0e7b8d49d890d35991ba86bf4f1e4f4a56b71a6
Mattias Andersson <mattias800@gmail.com>
2013-09-20 16:00:04 +0200

    Added number test that fails.

--- test/com/jexpect/ExpectTest.java
+++ test/com/jexpect/ExpectTest.java
@@ -1,45 +1,53 @@
 package com.jexpect;
 
 
-import org.junit.Test;
-
 import junit.framework.Assert;
+import org.junit.Test;
 
 public class ExpectTest {
 
-  @Test
-  public void Expect_True_To_Be_True() throws Exception {
-    try {
-      Expect.expect(true).toBeTrue();
-    } catch (Exception e) {
-      Assert.fail();
+    @Test
+    public void Expect_True_To_Be_True() throws Exception {
+        try {
+            Expect.expect(true).toBeTrue();
+        } catch (Exception e) {
+            Assert.fail();
+        }
     }
-  }
 
-  @Test
-  public void Expect_False_To_Be_True() throws Exception {
-    try {
-      Expect.expect(false).toBeTrue();
-      Assert.fail();
-    } catch (Exception e) {
+    @Test
+    public void Expect_False_To_Be_True() throws Exception {
+        try {
+            Expect.expect(false).toBeTrue();
+            Assert.fail();
+        } catch (Exception e) {
+        }
     }
-  }
 
-  @Test
-  public void Expect_True_To_Be_False() throws Exception {
-    try {
-      Expect.expect(true).toBeFalse();
-      Assert.fail();
-    } catch (Exception e) {
+    @Test
+    public void Expect_True_To_Be_False() throws Exception {
+        try {
+            Expect.expect(true).toBeFalse();
+            Assert.fail();
+        } catch (Exception e) {
+        }
     }
-  }
 
-  @Test
-  public void Expect_False_To_Be_False() throws Exception {
-    try {
-      Expect.expect(false).toBeFalse();
-    } catch (Exception e) {
-      Assert.fail();
+    @Test
+    public void Expect_False_To_Be_False() throws Exception {
+        try {
+            Expect.expect(false).toBeFalse();
+        } catch (Exception e) {
+            Assert.fail();
+        }
     }
-  }
+
+    @Test
+    public void Expect_Number_To_Be_Less_Than() throws Exception {
+        try {
+            Expect.expect(10).toBeLessThan(11);
+        } catch (Exception e) {
+            Assert.fail();
+        }
+    }
 }
