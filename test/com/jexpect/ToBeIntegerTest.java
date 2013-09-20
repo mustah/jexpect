e9ac08e8639d44b2c3c77000e4904825c78afa32
must <mustafa.tahir@etraveli.com>
2013-09-20 17:31:29 +0200

    more tests for greaterThan()

--- test/com/jexpect/ToBeIntegerTest.java
+++ test/com/jexpect/ToBeIntegerTest.java
@@ -25,4 +25,14 @@ public class ToBeIntegerTest {
   public void Expect_Number_To_Be_Greater_Than_Should_Not_Throw() throws Exception {
     expect(12).toBeGreaterThan(11);
   }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void Expect_Number_To_Be_Greater_Than_Throw_When_Actual_And_Expected_Are_The_Same() throws Exception {
+    expect(11).toBeGreaterThan(11);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void Expect_Number_To_Be_Greater_Than_Should_Throw() throws Exception {
+    expect(10).toBeGreaterThan(11);
+  }
 }
