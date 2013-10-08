d1b80f36b28e5fef55d849eb367a8c015d3887fe
must <mustafa.tahir@etraveli.com>
2013-10-08 23:14:58 +0200

    moved test to another new test class

--- test/com/jexpect/ToBeLongTest.java
+++ test/com/jexpect/ToBeLongTest.java
@@ -2,9 +2,7 @@ package com.jexpect;
 
 import org.junit.Test;
 
-import static com.jexpect.ExceptionHandler.getMessage;
 import static com.jexpect.Expect.expect;
-import static junit.framework.Assert.assertEquals;
 
 public class ToBeLongTest {
 
@@ -38,25 +36,4 @@ public class ToBeLongTest {
     expect(1L).toBeGreaterThan(9L);
   }
 
-  @Test
-  public void When_Expected_To_Be_Greater_Than_Fails_Then_Exception_Should_Have_Message() throws Exception {
-    assertEquals("Expected 5 to be greater than 10",
-                 getMessage(new Command() {
-                   @Override
-                   public void execute() {
-                     expect(5L).toBeGreaterThan(10L);
-                   }
-                 }));
-  }
-
-  @Test
-  public void When_Expected_To_Be_Greater_Than_Null_Fails_Then_Exception_Should_Have_Message() throws Exception {
-    assertEquals("Expected 5 to be greater than null",
-                 getMessage(new Command() {
-                   @Override
-                   public void execute() {
-                     expect(5L).toBeGreaterThan(null);
-                   }
-                 }));
-  }
 }
