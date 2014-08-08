d14cb95f69c1c54f3d4beef448beaf7c2346e3aa
must <mustafa.tahir@etraveli.com>
2014-08-08 19:31:44 +0200

    add more tests

--- test/com/jexpect/ToBeCollectionNotTest.java
+++ test/com/jexpect/ToBeCollectionNotTest.java
@@ -19,4 +19,19 @@ public class ToBeCollectionNotTest {
     expect(Collections.<Integer>emptyList()).not().toContain(10);
     assertNotNull(expect(NULL_COLLECTION).not().not());
   }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void Given_Nonempty_Collection_When_Expect_To_Contain_Item_Should_Fail() {
+    expect(NON_EMPTY_COLLECTION).toContain(4);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void Given_Empty_Collection_When_Expect_To_Contain_Item_Then_It_Should_Fail() {
+    expect(Collections.emptyList()).toContain(5);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void Given_Nonempty_Collection_When_Expect_To_Be_Empty_Then_It_Should_Fail() {
+    expect(NON_EMPTY_COLLECTION).toBeEmpty();
+  }
 }
