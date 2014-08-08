94b680c2ca4456779774502197f7c932086ea57f
must <mustafa.tahir@etraveli.com>
2014-08-08 19:03:27 +0200

    Move logical asserts to single test method instead of having separate for each.
    This still conforms to single assert rule, e.g. single "logical" assert.

--- test/com/jexpect/ToBeStringTest.java
+++ test/com/jexpect/ToBeStringTest.java
@@ -10,8 +10,19 @@ import static org.junit.Assert.assertNotNull;
 public class ToBeStringTest {
 
   @Test
-  public void To_Be_String() throws Exception {
+  public void Expect_Should_Not_Throw_Exception() throws Exception {
+    expect(NULL_STRING).toBeNull();
+    expect(EMPTY_STRING).toBeEmpty();
+    expect(NULL_STRING).not().toBe("a");
     expect("must").toBe("must");
+    expect("s").not().toBeEmpty();
+    expect("a").not().toBe("b");
+    expect("a").not().toBe(null);
+  }
+
+  @Test
+  public void Given_Any_String_Then_Consecutive_Expect_Not_Not_Functions_Should_Not_Be_Null() throws Exception {
+    assertNotNull(expect("foo").not().not());
   }
 
   @Test(expected = IllegalArgumentException.class)
@@ -29,11 +40,6 @@ public class ToBeStringTest {
     expect(NULL_STRING).toBe("mattias");
   }
 
-  @Test
-  public void To_Be_Null() throws Exception {
-    expect(NULL_STRING).toBeNull();
-  }
-
   @Test(expected = IllegalArgumentException.class)
   public void Expected_Empty_String_To_Be_Null_Should_Fail() throws Exception {
     expect(EMPTY_STRING).toBeNull();
@@ -44,19 +50,9 @@ public class ToBeStringTest {
     expect(NULL_STRING).toBeEmpty();
   }
 
-  @Test
-  public void Expect_Empty_To_Be_Empty() throws Exception {
-    expect(EMPTY_STRING).toBeEmpty();
-  }
-
   @Test(expected = IllegalArgumentException.class)
   public void Expect_Nonempty_To_Be_Empty_Should_Fail() throws Exception {
     expect("s").toBeEmpty();
-  }
-
-  @Test
-  public void Expect_Not_To_Be_Empty() throws Exception {
-    expect("s").not().toBeEmpty();
   }
 
   @Test(expected = IllegalArgumentException.class)
@@ -64,33 +60,13 @@ public class ToBeStringTest {
     expect(EMPTY_STRING).not().toBeEmpty();
   }
 
-  @Test
-  public void Expect_Actual_To_Not_Be_As_Expected() throws Exception {
-    expect("a").not().toBe("b");
-  }
-
   @Test(expected = IllegalArgumentException.class)
   public void Expect_Actual_To_Not_Be_As_Expected_Should_Fail_When_They_Are_Same() throws Exception {
     expect("a").not().toBe("a");
   }
 
-  @Test
-  public void Expect_Actual_As_Null_To_Not_Be_As_Expected() throws Exception {
-    expect(NULL_STRING).not().toBe("a");
-  }
-
-  @Test
-  public void Expect_Actual_To_Not_Be_Null() throws Exception {
-    expect("a").not().toBe(null);
-  }
-
   @Test(expected = IllegalArgumentException.class)
   public void Expect_Null_To_Be_Null_Should_Fail() throws Exception {
     expect(NULL_STRING).toBe(null);
-  }
-
-  @Test
-  public void Given_Any_String_Then_Consecutive_Expect_Not_Not_Functions_Should_Not_Be_Null() throws Exception {
-    assertNotNull(expect("foo").not().not());
   }
 }
